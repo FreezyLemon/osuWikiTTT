6006e2f7079ef4c4bfde679f9f25f14cc4210cae
FreezyLemon <freezestyleman@gmx.de>
2018-05-19 12:25:42 +0200

    Add project files.

---
+++ osuWikiTTT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace osuWikiTTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
